5c2b5aecd5692a2ef78b120adff4dadc89d56b29
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2024-08-03 09:29:21 -0500

    rofi config, updated firefox formatting

--- firefox.nix
+++ firefox.nix
@@ -11,15 +11,14 @@
     };
   in
 {
-  programs = {
-    firefox = {
-      enable = true;
-      languagePacks = [ "de" "en-US" ];
+  programs.firefox = {
+    enable = true;
+    languagePacks = [ "de" "en-US" ];
 
-      /* ---- POLICIES ---- */
-      # Check about:policies#documentation for options.
-      policies = {
-        DisablePocket = true;
+    /* ---- POLICIES ---- */
+    # Check about:policies#documentation for options.
+    policies = {
+      DisablePocket = true;
 	OfferToSaveLogins = false;
 	OverrideFirstRunPage = "";
 	OverridePostUpdatePage = "";
@@ -27,18 +26,18 @@
 	NewTabPage = false;
 	PromptForDownloadLocation = true;
 
-        /* ---- EXTENSIONS ---- */
-        # Check about:support for extension/add-on ID strings.
-        # Valid strings for installation_mode are "allowed", "blocked",
-        # "force_installed" and "normal_installed".
-        ExtensionSettings = {
-          # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-          # ublock origin
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "normal_installed";
+      /* ---- EXTENSIONS ---- */
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionSettings = {
+        # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        # ublock origin
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "normal_installed";
 	    default_area = "menupanel";
-          };
+        };
 	  # bitwarden
 	  "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
 	    install_url =  "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
@@ -63,18 +62,17 @@
 	    installation_mode = "normal_installed";
 	    default_area = "menubar";
 	  };
-        };
-  
-        /* ---- PREFERENCES ---- */
-        # Check about:config for options.
-        Preferences = { 
+      };
+
+      /* ---- PREFERENCES ---- */
+      # Check about:config for options.
+      Preferences = { 
 	  "browser.sessionstore.resume_from_crash" = false; # don't auto-restore after a crash
-          "extensions.pocket.enabled" = lock-false; # Disables Pocket
+        "extensions.pocket.enabled" = lock-false; # Disables Pocket
 	  "browser.theme.content-theme" = 0; # Dark Mode
 	  "browser.theme.toolbar-theme" = 0; # Dark Mode
 	  "toolkit.legacyUserProfileCustomizations.stylesheets" = lock-true; # Enable userChrome.css
 	  # "signon.rememberSignons" = lock-false; # Autofill
-        };
       };
     };
   };
