f5fd36424080b0de39907aa5bd86c1a6f535332e
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2024-08-11 12:08:38 -0500

    organizing

--- hosts/arion/home.nix
+++ hosts/arion/home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "usr";
   home.homeDirectory = "/home/usr";
 
@@ -15,37 +13,44 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
   imports = [
     ../../modules/btop.nix
     ../../modules/firefox.nix
   ];
 
   home.packages = with pkgs; [
-    neovim
     alacritty
-    python3
-    python312Packages.pip
-    thefuck
-    fastfetch
-    discord
-    google-chrome
-    teamviewer
-    ungoogled-chromium
-    cowsay
-    lolcat
-    obsidian
-    gpick
-    zoom-us
-    sops
-    orca-slicer
-    git-credential-manager
-    dig
-    rofimoji
     rofi-calc
     rofi-power-menu
-    xclip
-    inxi
+    rofimoji
 
+    # terminal apps
+    cowsay
+    dig
+    fastfetch
+    inxi
+    lolcat
+    neovim
+    python3
+    python312Packages.pip
+    sops
+    thefuck
+    xclip
+
+    # desktop apps
+    discord
+    google-chrome
+    obsidian
+    orca-slicer
+    teamviewer
+    ungoogled-chromium
+    zoom-us
+
+    # desktop utilities
+    gpick
     pavucontrol
   ];
 
@@ -116,28 +121,4 @@
       max-recent = 0
     '';
   };
-
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/usr/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
