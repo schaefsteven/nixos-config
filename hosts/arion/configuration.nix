4aff4e9370508e5d0cb163de21dfa5925b63acfc
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-05-07 22:31:40 -0400

    added shared smb share

--- hosts/arion/configuration.nix
+++ hosts/arion/configuration.nix
@@ -99,6 +99,21 @@
     ];
   };
 
+  fileSystems."/mnt/shared" = {
+    device = "//192.168.1.2/shared";
+    fsType = "cifs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "cred=/run/secrets/smb_credentials"
+      "uid=${toString config.users.users.usr.uid}"
+      "gid=${toString config.users.groups.users.gid}"
+    ];
+  };
+
   # game drive
   boot.supportedFilesystems = ["ntfs"];
 
