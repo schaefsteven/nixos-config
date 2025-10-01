69cb6f33c06fd9287485ae273f15ab333b09f21e
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-10-01 11:56:23 -0400

    tmux config

--- modules/hm-main-user-core/alacritty.nix
+++ modules/hm-main-user-core/alacritty.nix
@@ -1,74 +1,84 @@
 { config, pkgs, ... }:
 
 let 
-  # Use this to set the "scroll speed" of Ctrl+Shift+k/j in number of lines.
-  # It creates multiple bindings in the config file, which is the only way I
-  # know of to control the number of lines scrolled by the action.
-  scrollSpeed = 8;
+# Use this to set the "scroll speed" of Ctrl+Shift+k/j in number of lines.
+# It creates multiple bindings in the config file, which is the only way I
+# know of to control the number of lines scrolled by the action.
+scrollSpeed = 8;
 
-  scrollLineUp = {
-    key = "K";
-    mods = "Control|Shift";
-    action = "ScrollLineUp";
-  };
-  scrollLineDown = {
-    key = "J";
-    mods = "Control|Shift";
-    action = "ScrollLineDown";
-  };
+scrollLineUp = {
+  key = "K";
+  mods = "Control|Shift";
+  action = "ScrollLineUp";
+};
+scrollLineDown = {
+  key = "J";
+  mods = "Control|Shift";
+  action = "ScrollLineDown";
+};
 
   scrollBindings = builtins.concatLists [
-    (builtins.genList (_: scrollLineUp ) scrollSpeed)
-    (builtins.genList (_: scrollLineDown ) scrollSpeed)
+  (builtins.genList (_: scrollLineUp ) scrollSpeed)
+(builtins.genList (_: scrollLineDown ) scrollSpeed)
   ];
-in
+  in
 {
   programs.alacritty = {
     enable = true;
     settings = {
       window = {
-        # opacity = 0.95;
         padding = {
           x = 5;
-  	  y = 5;
-	};
+          y = 5;
+        };
       };
       keyboard.bindings = builtins.concatLists [
         [
           {
-    	    key = "N";
-    	    mods = "Control|Shift";
-    	    action = "SpawnNewInstance";
-	  }
-	  { 
-	    key = "U";
-	    mods = "Control|Shift";
-	    action = "ScrollHalfPageUp";
-	  }
-	  { 
-	    key = "D";
-	    mods = "Control|Shift";
-	    action = "ScrollHalfPageDown";
-	  }
-	]
+            key = "N";
+            mods = "Control|Shift";
+            action = "SpawnNewInstance";
+          }
+          { 
+            key = "U";
+            mods = "Control|Shift";
+            action = "ScrollHalfPageUp";
+          }
+          { 
+            key = "D";
+            mods = "Control|Shift";
+            action = "ScrollHalfPageDown";
+          }
+          # pass through ctrl-tab for tmux
+          {
+            key = "Tab";
+            mods = "Control";
+            chars = "\\u001b[27;5;9~";
+          }
+          {
+            key = "Tab";
+            mods = "Control|Shift";
+            chars = "\\u001b[27;6;9~";
+          }
+        ]
 
-	scrollBindings
+          scrollBindings
 
-      ];
-      # Hint Mode - Default keybind collides with my desired ScrollHalfPageUp
-      # For some reason, in order to change the keybind, I seem to have to completely
-      # re-define it. See https://alacritty.org/config-alacritty.html
-      hints.enabled = [
+          ];
+# Hint Mode - Default keybind collides with my desired ScrollHalfPageUp
+# For some reason, in order to change the keybind, I seem to have to completely
+# re-define it. See https://alacritty.org/config-alacritty.html
+        hints.enabled = [
         {
           binding = { key = "H"; mods = "Control|Shift"; };
-	  command = "xdg-open";
+          command = "xdg-open";
           hyperlinks = true;
           post_processing = true;
           persist = false;
           mouse.enabled = true;
           regex = "(ipfs:|ipns:|magnet:|mailto:|gemini://|gopher://|https://|http://|news:|file:|git://|ssh:|ftp://)[^\\u0000-\\u001F\\u007F-\\u009F<>\"\\\\s{-}\\\\^⟨⟩`]+";
-	}
-      ];
+        }
+        ];
     };
   };
 }
--- modules/hm-main-user-core/tmux.nix
+++ modules/hm-main-user-core/tmux.nix
@@ -5,5 +5,9 @@
     enable = true;
     mouse = true;
     keyMode = "vi";
+    plugins = [ pkgs.tmuxPlugins.vim-tmux-navigator ];
+    extraConfig = ''
+      bind-key -n C-Tab next-window
+    '';
   };
 }
