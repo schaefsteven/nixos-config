a7deaa553b0bf0b8299b3a846e376042926b7d12
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2026-03-01 15:15:47 -0500

    added ollama model

--- hosts/arion/ollama.nix
+++ hosts/arion/ollama.nix
@@ -3,7 +3,7 @@
 {
   services.ollama = {
     enable = true;
-    loadModels = ["deepseek-r1:14b"];
+    loadModels = ["deepseek-r1:14b" "deepseek-coder:6.7b"];
     openFirewall = true;
     host = "0.0.0.0";
     package = pkgs.ollama-rocm;
